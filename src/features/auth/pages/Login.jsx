0d831a05060290a8c63fab95308d43b09ac92f77
jhoan636 <jhoan.63@hotmail.com>
2025-04-03 14:32:10 -0500

    feat(components):add reusable Input
    Implement reusable input for forms

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../services/authService"; 
-import logo from '../../../assets/images/PapperTech_logo_colored_text.png';
+import { login } from "../services/authService";
+import logo from "../../../assets/images/PapperTech_logo_colored_text.png";
+import Input from "../../../components/forms/Input";
 import "../styles/Login.css";
-
-
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -26,45 +25,50 @@ const Login = () => {
   };
 
   return (
-  <div className="login-container">
-    {/* Sección de la imagen */}
-    <div className="image-section">
-      <div className="image-placeholder">
-        <img src={logo} alt="Logo" />
+    <div className="login-container">
+      {/* Sección de la imagen */}
+      <div className="image-section">
+        <div className="image-placeholder">
+          <img src={logo} alt="Logo" />
+        </div>
+      </div>
+
+      {/* Sección del formulario */}
+      <div className="form-section">
+        <h2>Log In</h2>
+        {error && (
+          <p className="error" style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
+        <form onSubmit={handleSubmit}>
+          <Input
+            label="Username"
+            name="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+           <Input
+            label="Password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />    
+          <p className="info-text">
+            It must be a combination of at least 8 letters, numbers, and
+            symbols.
+          </p>
+          <div className="links">
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button type="submit">Log In</button>
+        </form>
       </div>
     </div>
-
-    {/* Sección del formulario */}
-    <div className="form-section">
-      <h2>Log In</h2>
-      {error && <p className="error" style={{ color: "red" }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Username</label>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <p className="info-text">
-          It must be a combination of at least 8 letters, numbers, and symbols.
-        </p>
-        <div className="links">
-          <a href="#">Forgot Password?</a>
-        </div>
-        <button type="submit">Log In</button>
-      </form>
-    </div>
-  </div>
   );
 };
 
