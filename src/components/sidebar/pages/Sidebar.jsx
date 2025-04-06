7bbd543f650b530badc327064766c4c7b9f8b63f
jhoan636 <jhoan.63@hotmail.com>
2025-04-06 18:25:39 -0500

    refactor(sidebar): improve Sidebar component structure and styles
    Enhanced Sidebar.module.css with hover effects and improved layout

--- src/components/sidebar/pages/Sidebar.jsx
+++ src/components/sidebar/pages/Sidebar.jsx
@@ -1,37 +1,38 @@
-import { UserSearch } from 'lucide-react';
+import { UserSearch } from "lucide-react";
 import logo from "../../../assets/images/PepperTech_logo_sin_fondo.png";
 import styles from "../styles/Sidebar.module.css";
+import { UserPlus, Users, Handshake, Package, Building2, FileText, CheckSquare, Phone, Video, File, Calendar } from "lucide-react";
 
 const Sidebar = () => {
   const menuItems = [
-    { name: "Leads" },
-    { name: "Contacts" },
-    { name: "Deals" },
-    { name: "Products" },
-    { name: "Vendors" },
-    { name: "Quotes" },
-    { name: "Task" },
-    { name: "Calls" },
-    { name: "Meets" },
-    { name: "Forms" },
-    { name: "Calendar" },
+    { name: "Leads", icon: <UserPlus size={24} strokeWidth={1.5} /> },
+    { name: "Contacts", icon: <Users size={24} strokeWidth={1.5} /> },
+    { name: "Deals", icon: <Handshake size={24} strokeWidth={1.5} /> },
+    { name: "Products", icon: <Package size={24} strokeWidth={1.5} /> },
+    { name: "Vendors", icon: <Building2 size={24} strokeWidth={1.5} /> },
+    { name: "Quotes", icon: <FileText size={24} strokeWidth={1.5} /> },
+    { name: "Task", icon: <CheckSquare size={24} strokeWidth={1.5} /> },
+    { name: "Calls", icon: <Phone size={24} strokeWidth={1.5} /> },
+    { name: "Meets", icon: <Video size={24} strokeWidth={1.5} /> },
+    { name: "Forms", icon: <File size={24} strokeWidth={1.5} /> },
+    { name: "Calendar", icon: <Calendar size={24} strokeWidth={1.5} /> },
   ];
 
   return (
     <aside className={styles.sidebar}>
-      <div className="logo">
-        <span className="logo-icon">
+      <div className={styles.logo}>
+        <span className={styles.logoIcon}>
           <img src={logo} alt="PepperTech" />
         </span>
-        <h1 className="logo-text">PepperTech</h1>
+        <h1 className={styles.logoText}>PepperTech</h1>
       </div>
-      <nav className="menu">
-        <p className="section-title">Main</p>
+      <nav className={styles.menu}>
+        <p className={styles.sectionTitle}>Main</p>
         <ul>
           {menuItems.map((item) => (
             <li key={item.name}>
-              <UserSearch size={24} strokeWidth={1} />
-              <span>{item.name}</span>
+              <span>{item.icon}</span>
+              <a href={item.name}>{item.name}</a>
             </li>
           ))}
         </ul>
