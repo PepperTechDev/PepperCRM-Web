56d7983b2cc6609b9fb96c49bb7feab65201d9d3
jhoan636 <jhoan.63@hotmail.com>
2025-04-11 12:30:05 -0500

    feat(vendors): implement VendorsService with CRUD operations for vendor management

---
+++ src/features/vendors/services/VendorsService.js
@@ -0,0 +1,21 @@
+import api from "../../../services/ApiClient";
+
+export const getVendors = async () => {
+  const response = await api.get("/Vendors");
+  return response.data;
+};
+
+export const createVendors = async (Vendor) => {
+  const response = await api.post("/Vendors", Vendor);
+  return response.data;
+};
+
+export const updateVendors = async (id, Vendor) => {
+  const response = await api.put(`/Vendors/${id}`, Vendor);
+  return response.data;
+};
+
+export const deleteVendors = async (id) => {
+  const response = await api.delete(`Vendors/${id}`);
+  return response.data;
+};
