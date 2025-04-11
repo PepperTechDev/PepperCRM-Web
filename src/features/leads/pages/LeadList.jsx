3cb79cf6b4e26bbb3727a9b5ae06c548ba7cb28e
jhoan636 <jhoan.63@hotmail.com>
2025-04-11 00:52:44 -0500

    feat(leads): update user data structure and table headers in LeadList component
    Updated LeadList table headers to reflect latest requirements

--- src/features/leads/pages/LeadList.jsx
+++ src/features/leads/pages/LeadList.jsx
@@ -1,19 +1,18 @@
 import DataListItem from "../../../components/dataListItem/pages/DataListItem";
 
 const userData = [
-  { id: 1, name: "Jhoan Esteban Londoño Escobar", email: "devlondono@gmail.com" },
-  { id: 2, name: "Jane Smith", email: "jane@example.com" },
-  { id: 2, name: "Jane Smith", email: "jane@example.com" },
-  { id: 4, name: "Jane Smith", email: "jane@example.com" },
-
+  { id: 1, name: "Jhoan Esteban", lastname: "Londoño Escobar",email: "devlondono@gmail.com", phone: "3192061970", service: "Service 1" },
+  { id: 2, name: "Jorge Andrés", lastname: "Rojas Sepúlveda",email: "Jorge_rojas82212@elpoli.edu.co", phone: "323 5312623", service: "Service 1" }
 ];
 
 const renderHeaders = () => (
   <>
-    <th>Cedula</th>
+    <th>Id</th>
     <th>Name</th>
+    <th>Lastname</th>
     <th>Email</th>
-    <th>Prueba</th>
+    <th>Phone</th>
+    <th>Service</th>
   </>
 );
 
@@ -21,8 +20,10 @@ const renderRow = (item) => (
   <>
     <td>{item.id}</td>
     <td>{item.name}</td>
+    <td>{item.lastname}</td>
     <td>{item.email}</td>
-    <td>{item.prueba}</td>
+    <td>{item.phone}</td>
+    <td>{item.service}</td>
   </>
 );
 
