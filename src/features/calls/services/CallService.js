a171099ded47220d2b0e16d070ef4e3848157d29
jguerreromira <jguerreromira@gmail.com>
2025-04-11 09:49:36 -0500

    feat(calls): implement CallService for managing calls

---
+++ src/features/calls/services/CallService.js
@@ -0,0 +1,21 @@
+import api from "../../../services/ApiClient";
+
+export const getCalls = async () => {
+  const response = await api.get("/Calls");
+  return response.data;
+};
+
+export const createCalls = async (contact) => {
+  const response = await api.post("/Calls", contact);
+  return response.data;
+};
+
+export const updateCalls = async (id, contact) => {
+  const response = await api.put(`/Calls/${id}`, contact);
+  return response.data;
+};
+
+export const deleteCalls = async (id) => {
+  const response = await api.delete(`/Calls/${id}`);
+  return response.data;
+};
