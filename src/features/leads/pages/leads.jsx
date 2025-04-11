40ada489da1847bc720a98f50310a4c0bb1fdf6b
BillyowRojas <jorgeandres14190@gmail.com>
2025-04-11 00:12:20 -0500

    fix(leads):imports were fixed
    changed some name's files to match the importations on other components

--- src/features/leads/pages/leads.jsx
+++ src/features/leads/pages/leads.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import styles from"../styles/Leads.modules.css";
+import styles from"../styles/Leads.module.css";
+import Sidebar from "../../../components/sidebar/pages/Sidebar";
 import LeadsTable from "../../../components/leads/LeadsTable";
+import Navbar from "../../../components/navbar/pages/Navbar";
 function Leads() {
   return (
     <section className={styles.containerLeads}>
+      <Sidebar />
+      <div className={styles.flexLeads}>
+      <Navbar />
       <LeadsTable />
-
+      </div>
     </section>
   );
 }
