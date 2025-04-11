a37edc3714a29dd44267ec60292c4ee30b28093b
jhoan636 <jhoan.63@hotmail.com>
2025-04-11 12:01:46 -0500

    feat(navbar): enhance breadcrumb navigation with dynamic links and styles

--- src/components/navbar/pages/Navbar.jsx
+++ src/components/navbar/pages/Navbar.jsx
@@ -1,13 +1,37 @@
+import { useLocation, Link } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
-import { Search, CircleHelp,CircleUser   } from "lucide-react";
+import { Search, CircleHelp, CircleUser } from "lucide-react";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  // Divide la ruta actual en segmentos
+  const pathSegments = location.pathname
+    .split("/")
+    .filter((segment) => segment);
   return (
     <nav className={styles.headerBar}>
       <div className={styles.breadcrumb}>
-        <span className={styles.section}>Meets</span>
-        <span className={styles.separator}>&gt;</span>
-        <span className={styles.current}>All</span>
+      {pathSegments.map((segment, index) => {
+          const path = `/${pathSegments.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathSegments.length - 1;
+
+          return (
+            <span
+              key={path}
+              className={styles.section}
+            >
+              {!isLast ? (
+                <Link to={path} className={styles.link}>
+                  {segment.charAt(0).toUpperCase() + segment.slice(1)}
+                </Link>
+              ) : (
+                segment.charAt(0).toUpperCase() + segment.slice(1)
+              )}
+              {!isLast && <span className={styles.separator}>&gt;</span>}
+            </span>
+          );
+        })}
       </div>
       <div className={styles.searchWrapper}>
         <Search size={24} strokeWidth={1.5} />
@@ -18,8 +42,8 @@ const Navbar = () => {
         />
       </div>
       <div className={styles.icons}>
-        <CircleHelp className={styles.iconButton}  size={24} strokeWidth={1.5} />
-        <CircleUser className={styles.iconButton}  size={24} strokeWidth={1.5} />
+        <CircleHelp className={styles.iconButton} size={24} strokeWidth={1.5} />
+        <CircleUser className={styles.iconButton} size={24} strokeWidth={1.5} />
       </div>
     </nav>
   );
