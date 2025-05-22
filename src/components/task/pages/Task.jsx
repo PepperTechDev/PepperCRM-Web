b2eb152af81dd8e669d741fb1f8aacb65493e3e6
jhoan636 <jhoan.63@hotmail.com>
2025-05-22 14:07:01 -0500

    feat: add onEditTask and onDeleteTask handlers to Task component

--- src/components/task/pages/Task.jsx
+++ src/components/task/pages/Task.jsx
@@ -1,18 +1,44 @@
-import { useDraggable } from '@dnd-kit/core';
-import styles from '../styles/Task.module.css';
+import { useDraggable } from "@dnd-kit/core";
+import { Pencil, Trash2 } from "lucide-react";
+import styles from "./../styles/Task.module.css";
 
-function Task({ task }) {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+function Task({ task, onEditTask, onDeleteTask }) {
+  const { attributes, listeners, setNodeRef } = useDraggable({
     id: task.id,
   });
 
-  const style = {
-    transform: transform ? `translate(${transform.x}px, ${transform.y}px)` : '',
-  };
-
   return (
-    <div className={styles.task} ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      {task.content}
+    <div
+      ref={setNodeRef}
+      className={styles.task}
+      style={{ cursor: "grab" }}
+    >
+      {/* Solo el texto es draggable */}
+      <span {...listeners} {...attributes} style={{ flex: 1, cursor: "grab" }}>
+        {task.content}
+      </span>
+      <div className={styles.taskActions}>
+        <button
+          className={styles.iconBtn}
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            onEditTask(task);
+          }}
+        >
+          <Pencil size={16} />
+        </button>
+        <button
+          className={styles.iconBtn}
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            onDeleteTask(task);
+          }}
+        >
+          <Trash2 size={16} />
+        </button>
+      </div>
     </div>
   );
 }
