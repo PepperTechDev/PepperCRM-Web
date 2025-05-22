346cd3d793ebfeacc915191ec9db0fd9220d74be
jguerreromira <jguerreromira@gmail.com>
2025-05-22 11:23:26 -0500

    refactor: remove unused LeadsTable import from leads component

--- src/features/leads/pages/leads.jsx
+++ src/features/leads/pages/leads.jsx
@@ -1,6 +1,5 @@
 import styles from"../styles/Leads.module.css";
 import Sidebar from "../../../components/sidebar/pages/Sidebar";
-import LeadsTable from "../../../components/leads/LeadsTable";
 import Navbar from "../../../components/navbar/pages/Navbar";
 import Table from "./LeadList"
 function Leads() {
